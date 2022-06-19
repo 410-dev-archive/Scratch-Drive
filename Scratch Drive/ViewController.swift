b26b9aec044a1b3d69f286789b559d8778251b06
410 <me@hysong.dev>
2022-06-20 04:19:11 +0900

    Initial Commit

---
+++ Scratch Drive/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Scratch Drive
+//
+//  Created by Hoyoun Song on 2022/06/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
